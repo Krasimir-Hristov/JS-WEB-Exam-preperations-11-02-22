245fa944c32bd66594be5da7b4eb485ccbb93cfb
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-09-15 22:06:34 +0200

    a

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,9 +11,14 @@ router.get('/register', isGuest(), (req, res) => {
 
 router.post('/register', isGuest(), async (req, res) => {
     try {
-        if (req.body.password != req.body.repass) {
-            throw new Error('Passwords dont match !');
+        if( req.body.password.trim() == '') {
+            throw new Error('Passwords ist required !');
+
+        
+        }else if (req.body.password != req.body.repass) {
+            throw new Error('Passwords don\'t match !');
         }
+        
         const user = await register(req.body.firstName, req.body.lastName, req.body.email, req.body.password);
         req.session.user = user;
         res.redirect('/'); 
--- models/User.js
+++ models/User.js
@@ -1,17 +1,34 @@
-const { Schema, model }  = require('mongoose')
+const { Schema, model } = require('mongoose')
 
 //TODO add validations
 
 const NAME_PATTAERN = /^[a-zA-Z-]+$/;
+const EMAIL_PATTAERN = /^([a-zA-Z]+)@([a-zA-Z]+)\.([a-zA-Z]+)$/;
 const userSchema = new Schema({
-    firstName: { type: String, require: true, minLength: [3, 'First name must be at least 3 characters long'], validate: {
+    firstName: {
+        type: String, minlength: [3, 'First name must be at least 3 characters long'], validate: {
+            validator(value) {
+                return NAME_PATTAERN.test(value);
+            },
+            message: 'First name may contain only english letters'
+        }
+    },
+    lastName: {
+        type: String, minlength: [5, 'Last name must be at least 5 characters long'], validate: {
+            validator(value) {
+                return NAME_PATTAERN.test(value);
+            },
+            message: 'Last name may contain only english letters'
+        }
+    },
+    email: { 
+        type: String, required: [true, 'Email is required'], validate: {
         validator(value) {
-            return NAME_PATTAERN.test(value);
+            return EMAIL_PATTAERN.test(value);
         },
-        message: 'First name may contain only english letters'
-    } },
-    lastName: { type: String, require: true },
-    email: { type: String, require: true },
+        message: 'Email must be valid and may contain only english letters'
+    } 
+},
     hashedPassword: { type: String, require: true }
 });
 
